8d98ca7fb4a7db54e2740067ddb038a3c25bf68e
Akash520820 <chakrabortyakash731@gmail.com>
2026-09-25 09:31:40 +0530

    fix: set base path in vite.config.ts and provide SPA redirect script in 404.html to resolve blank page on refresh

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/greencards-admin-portal/',
   server: {
     port: 5175,
     open: true,
